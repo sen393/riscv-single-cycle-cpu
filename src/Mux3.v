7813934c9325c39b77c72ca0ec47d87fe265c97b
Kevin Zhang <kevinzhanggm1879@gmail.com>
2026-09-18 23:24:21 -0400

    added 3:1 multiplexer for jal (replaced ResultSrc mux)

---
+++ src/Mux3.v
@@ -0,0 +1,14 @@
+module Mux3
+(
+    input  [1:0]  sel,
+    input  [31:0] a,
+    input  [31:0] b,
+    input  [31:0] c,
+    output [31:0] out
+);
+
+    assign out = (sel == 2'b10) ? c :
+                 (sel == 2'b01) ? b :
+                 (sel == 2'b00) ? a : 1'bx;
+
+endmodule
